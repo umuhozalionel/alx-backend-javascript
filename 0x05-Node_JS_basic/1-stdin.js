77faa9302da2eed9b4235b46958ea6517f2b3c51
umuhozalionel <muhozalionel@gmail.com>
2024-11-26 18:21:57 +0000

    Initial commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
-});
+process.stdin.setEncoding('utf-8');
 
-process.on('exit', () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name.trim()}\n`);
+    process.stdout.write("This important software is now closing\n");
+  }
 });
-

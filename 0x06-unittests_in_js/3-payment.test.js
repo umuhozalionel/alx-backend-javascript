80e0a0b0000adab6342079f21190d8ecfca9c331
umuhozalionel <muhozalionel@gmail.com>
2024-11-30 03:07:25 +0000

    Initial commit

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,20 +1,25 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-
 const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi function', () => {
-  // create the spy / wrapper of an existing function
-  const utilSpy = sinon.spy(Utils, 'calculateNumber');
+  let utilSpy;
+
+  // Setup the spy before each test
+  beforeEach(() => {
+    utilSpy = sinon.spy(Utils, 'calculateNumber');
+  });
+
+  // Restore the spy after each test
+  afterEach(() => {
+    sinon.restore();
+  });
 
   it('validate the usage of the Utils function', () => {
-    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
-    // check that the spy was called exactly once
+    
     expect(utilSpy.calledOnce).to.be.true;
-    // check that spy was called at least once with the provided arguments
     expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
-    utilSpy.restore()
   });
 });
